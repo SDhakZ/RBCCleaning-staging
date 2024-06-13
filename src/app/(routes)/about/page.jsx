4324f6424643e6882d4cc83c0a92b58dae8f7122
Sharnam <sharnam34@gmail.com>
2024-06-13 11:46:55 +0545

    Faq merged

--- src/app/(routes)/about/page.jsx
+++ src/app/(routes)/about/page.jsx
@@ -2,9 +2,8 @@ import React from "react";
 import About from "./about.jsx";
 import ExtraInfo from "./Components/extraInfo.jsx";
 import ContactModule from "@/app/components/contactModule/contactModule";
-import Faq from "./Components/faq.jsx";
+import Faq from "@/app/components/faq/faq.jsx";
 import Services from "@/app/components/services/services.jsx";
-
 
 export default function page() {
   return (
